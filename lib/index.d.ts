2b5ed97ad5f985847d0831c2f9644c64e01ebd8d
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-08-08 23:57:20 +0900

    build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,8 @@
 import * as Tart from '@star__hoshi/tart';
 import * as firebase from 'firebase';
+declare type Partial<T> = {
+    [P in keyof T]?: T[P];
+};
 export declare const initialize: (_firestore: firebase.firestore.Firestore) => void;
 export declare class Snapshot<T extends Tart.Timestamps> {
     ref: firebase.firestore.DocumentReference;
@@ -7,6 +10,7 @@ export declare class Snapshot<T extends Tart.Timestamps> {
     constructor(ref: firebase.firestore.DocumentReference, data: T);
     constructor(queryDocumentSnapshot: firebase.firestore.QueryDocumentSnapshot);
     constructor(documentSnapshot: firebase.firestore.DocumentSnapshot);
+    readonly firestoreURL: string | undefined;
     private setCreatedDate;
     refresh(): Promise<void>;
     save(): Promise<void>;
@@ -16,14 +20,11 @@ export declare class Snapshot<T extends Tart.Timestamps> {
     saveNestedCollection<S extends Tart.Timestamps>(collectionName: string, snapshot: Snapshot<S>): Promise<void>;
     saveNestedCollectionWithBatch<S extends Tart.Timestamps>(batch: firebase.firestore.WriteBatch, collectionName: string, snapshot: Snapshot<S>): void;
     fetchNestedCollections<S extends Tart.Timestamps>(collectionName: string): Promise<Snapshot<S>[]>;
-    update(data: {
-        [id: string]: any;
-    }): Promise<void>;
-    updateWithBatch(batch: firebase.firestore.WriteBatch, data: {
-        [id: string]: any;
-    }): void;
+    update(data: Partial<T>): Promise<void>;
+    updateWithBatch(batch: firebase.firestore.WriteBatch, data: Partial<T>): void;
     delete(): Promise<void>;
     deleteWithBatch(batch: firebase.firestore.WriteBatch): void;
 }
 export declare const makeNotSavedSnapshot: <T extends Tart.Timestamps>(path: string, data: T, id?: string | undefined) => Snapshot<T>;
 export declare const fetch: <T extends Tart.Timestamps>(pathOrDocumentReference: string | firebase.firestore.DocumentReference, id?: string | undefined) => Promise<Snapshot<T>>;
+export {};
